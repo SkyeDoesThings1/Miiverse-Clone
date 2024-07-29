af275da0651830013c2f095367634e301be5dd91
SkyeDoesThings1 <monteleone67774@gmail.com>
2024-07-28 23:02:04 -0400

    Created useless frontend

---
+++ public/portal/js/portal.js
@@ -0,0 +1,3 @@
+function stopLoading() {
+  wiiuBrowser.endStartUp();
+}
--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const subdomain = require('express-subdomain');
+const path = require('path');
 const colors = require('colors');
 const discovery = require('./routes/disc-olv');
 const config = require('./config.json');
 const app = express();
 const port = config.port;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public', 'views'));
+app.use(express.static('public'));
 
 try {
   app.use(subdomain('disc.olv', discovery));
